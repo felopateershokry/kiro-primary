e57c313cabb6c4b965b8d1b84e9cff4303354c3c
felopateershokry <143431396+felopateershokry@users.noreply.github.com>
2025-11-03 21:53:45 +0200

    wait till photos

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -118,7 +118,7 @@ export const dummyKhodam = [
         "instructor": {
             "_id": "100",
             "name": "ابانوب مكرم",
-            "imageUrl" : "https://scontent.fcai19-8.fna.fbcdn.net/v/t39.30808-1/503983540_3894744670672115_8715881428891033994_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=106&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeGQYK0WzHckuGhvoP0NaiiZAwBOLWkcof4DAE4taRyh_qI-xL5LfdZWKFlbq3Q01MzOGozdN90NX_bqnA7Qp-Ai&_nc_ohc=HEo2uYTPhjoQ7kNvwEsOtlF&_nc_oc=AdklVy6CYgbymYcZcYTIa6qZEAVc3a864U0wZw8NoD4OShLHPk_Z9MlnYeKGDibb5rs&_nc_zt=24&_nc_ht=scontent.fcai19-8.fna&_nc_gid=2gJ5PnJKevYHB3DVoYUibA&oh=00_Afcp7cfLWDFT5MAibXKqmc1g_N3su8j-FSG3V1wzXYT_NA&oe=68FFCAB4",
+            "imageUrl" : home,
             "phone": "01280511848",
             "address": "Alexandria, Egypt",
             "notes": "امين الخدمة",
@@ -128,7 +128,7 @@ export const dummyKhodam = [
         "instructor": {
             "_id": "101",
             "name": "كيرلس شكري",
-            "imageUrl" :"https://scontent.fcai19-8.fna.fbcdn.net/v/t39.30808-6/454778738_3866136360273272_3172187173891861601_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF0G2tsSgK3EzQ2NrzMQLzxMZii83rljiAxmKLzeuWOIH7N6c5dftvlrPUxpnRZeRA9M82PDKhdL_HoR95SSj83&_nc_ohc=9xCswj8ghVwQ7kNvwE3inRx&_nc_oc=AdlxOD1kz9Vh9bbpYuO1xKeqjYzzRUvB_PAZT76s1sMh_MigX-xG86X03pjE9-uWWe8&_nc_zt=23&_nc_ht=scontent.fcai19-8.fna&_nc_gid=SGhExKD9OEYXk55wJo7Evg&oh=00_AfedjiI6hXvjiOQ-wVp-nkp1k3w-bik7dKM6OjCXx6QSUQ&oe=68FFDF87",
+            "imageUrl" :home,
             "phone": "01225525117",
             "address": "Alexandria, Egypt",
         },
@@ -137,7 +137,7 @@ export const dummyKhodam = [
         "instructor": {
             "_id": "102",
             "name": "بيشوي يوسف",
-            "imageUrl" : "https://scontent.fcai19-8.fna.fbcdn.net/v/t39.30808-1/421114233_3021161981358967_7664947830573197568_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=107&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeFQYBQt2Cy3HZeXYfE_74Z_Zy-FYNGYGiRnL4Vg0ZgaJCJiLKBKJ6ooE0Tb8HDKAOTd6Yk8Ulki6M3IwILOB2Hd&_nc_ohc=3qR6vq8cYcwQ7kNvwFfxGRo&_nc_oc=AdnSceZg4H7iUOXgGO3JZbuIcQ2r-oacp86Qsoe6S1xbY7CXPBz-6wzIu0ZbtrdJj3A&_nc_zt=24&_nc_ht=scontent.fcai19-8.fna&_nc_gid=qxYD2g-XckFxQD3uzDQQzQ&oh=00_AfckthLe_7oikuJj-Lk85_VTC8_caTSZTV1u9LuBBINnzg&oe=68FFDC0E",
+            "imageUrl" : home,
             "phone": "01121015539",
             "address": "Alexandria, Egypt",
         },
@@ -146,7 +146,7 @@ export const dummyKhodam = [
         "instructor": {
             "_id": "103",
             "name": "مينا رافت",
-            "imageUrl" : "https://scontent.fcai19-8.fna.fbcdn.net/v/t39.30808-1/566327067_2343452402741376_7052232656135698105_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=102&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeE9PmaKv0sGlzpSJwpWtdKw1tFymo9gpwfW0XKaj2CnB5YqKb4DN6x_axhRPusx_JhH0GCZ0L6E9AAPTYZOXR0P&_nc_ohc=fcRX82ZinPIQ7kNvwHTi29A&_nc_oc=AdnoCGraJvRAnZLt4eU3wxLjFhF-fXDWwV6ScGt5v06atckXnGtepWW0Btg8nYOZv3E&_nc_zt=24&_nc_ht=scontent.fcai19-8.fna&_nc_gid=hs4oElx_m6PRPvjjum8l4g&oh=00_AfdztJBRQTQ7-uqZBmQQtAbrUGrpjSmOWy1Eevhnmn_nyA&oe=68FFE409",
+            "imageUrl" : home,
             "phone": "01200908588",
             "address": "Alexandria, Egypt",
         },
@@ -155,7 +155,7 @@ export const dummyKhodam = [
         "instructor": {
             "_id": "104",
             "name": "كيرلس اسحاق",
-            "imageUrl" : "https://scontent.fcai19-8.fna.fbcdn.net/v/t39.30808-1/492001827_1202446877885990_8375485053466693229_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=108&ccb=1-7&_nc_sid=1d2534&_nc_eui2=AeGsxtQ2TCwo58wO5J07c-jbYKitTWyNxghgqK1NbI3GCPvIiTSEWNsGAG9fF34PaiulEPksc62TIAUWAn8QUkiP&_nc_ohc=w7GAB_bqx08Q7kNvwFAZRhI&_nc_oc=AdniwR5e3hZAdBLoQbfOYGjDrZ0xmDAeSGJOreYoT3L-LOTupVItDJWhJ2oSglGBALs&_nc_zt=24&_nc_ht=scontent.fcai19-8.fna&_nc_gid=RPNcAkD1NhJdrSHJHUj9Tw&oh=00_Afd3aUF5CH4a03H2w99pndVl19IQDyRCqq_So8j6msI7YQ&oe=68FFD625",
+            "imageUrl" : home,
             "phone": "01275987259",
             "address": "Alexandria, Egypt",
         },
